99419a244d6c4d386584753b6edd042b037ed1b9
Canaan West <canaan.west@gmail.com>
2017-11-27 15:17:31 -0800

    Songs displaying on page

--- src/app.js
+++ src/app.js
@@ -1,13 +1,59 @@
 // Vendor Modules
+/*eslint-disable*/
 import $ from 'jquery';
 import _ from 'underscore';
 
 // CSS
 import './css/foundation.css';
 import './css/style.css';
+import Song from './models/song'
+
+//hard coded data
+let songTemplate;
+const songData = [
+    {
+      title: "Drop It Like It's Hot",
+      year: 2004,
+      artist: "Snoop Dog"
+    },
+    {
+      title: "On and Off",
+      year: 2017,
+      artist: "Maggie Rogers"
+    },
+    {
+      title: "Suit and Jacket",
+      year: 2016,
+      artist: "Judah and the Lion"
+    },
+  ];
 
 console.log('it loaded!');
+//
 
+// create a new instance of song using the model
+const alaska = new Song({
+  title: "Alaska",
+  year: 2017,
+  artist: "Maggie Rogers"
+})
+
+console.log(alaska)
+// render function
+
+
+// document ready
 $(document).ready( () => {
-  
+  songTemplate = _.template($('#song-template').html());
+  songData.forEach((song) => {
+    $('#song-list').append(songTemplate(song))
+  })
 });
+
+
+//INSTRUCTIONS
+
+// There is an Underscore template stub already created for you in the dist/index.html file. Update this template to display some of the song fields that you want to see displayed.
+// Compile the template in app.js in the appropriate section of code
+// Invoke the compiled template to generate the HTML for the model instance created above
+// Add the song's HTML to the DOM (check out index.html to see where you'd like to add it)
